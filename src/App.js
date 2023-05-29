310cb6a4edef88d50a3c91005e1ea5bf5b0ed8ce
sam11212 <samuellegesse11212@gmail.com>
2023-05-29 22:48:37 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Container from './componenets/Container';
+
+function App() {
+  console.log("App.js")
+  return (
+    <div className="base">
+      <Container />
+    </div>
+  );
+}
+
+export default App;
